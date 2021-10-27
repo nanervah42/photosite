953fe57f425f0cdc81eb534714be9cf8c6253e23
nanervah <nanervah@yandex.ru>
2021-10-28 01:16:16 +0700

    #2

--- photography/photo/urls.py
+++ photography/photo/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('', HomePhoto.as_view(), name='home'),
-    path('', index, name='index'),
+    path('', HomePhoto.as_view(), name='home'),
     ]
--- photography/photo/views.py
+++ photography/photo/views.py
@@ -5,10 +5,9 @@ from django.views.generic import TemplateView
 class HomePhoto(TemplateView):
     template_name = 'photo/index.html'
     context_object_name = 'photo'
+    title = 'test'
 
-
-def index(request):
-    context = {
-        'title': 'test'
-    }
-    return render(request, 'photo/index.html', context)
+    def get_context_data(self, *, object_list=None, **kwargs):  # ...для динамичных данных
+        context = super().get_context_data(**kwargs)            # получаем контекст, который уже есть
+        context['title'] = 'Главная страница'                   # дополняем его
+        return context
