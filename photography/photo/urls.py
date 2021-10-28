9f04e6c7fc563bc899db77a6d2cc7458ff8b014a
nanervah <nanervah@yandex.ru>
2021-10-28 17:55:43 +0700

    #3

--- photography/photo/urls.py
+++ photography/photo/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', HomePhoto.as_view(), name='home'),
+    path('price/', Price.as_view(), name='price'),
+    path('collection/', Collection.as_view(), name='collection'),
+    path('contacts/', Contacts.as_view(), name='contacts'),
+    path('about/', About.as_view(), name='about'),
     ]
--- photography/photo/views.py
+++ photography/photo/views.py
@@ -5,9 +5,49 @@ from django.views.generic import TemplateView
 class HomePhoto(TemplateView):
     template_name = 'photo/index.html'
     context_object_name = 'photo'
-    title = 'test'
 
-    def get_context_data(self, *, object_list=None, **kwargs):  # ...для динамичных данных
-        context = super().get_context_data(**kwargs)            # получаем контекст, который уже есть
-        context['title'] = 'Главная страница'                   # дополняем его
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        context['home_under_line'] = 'class="colorlib-active"'
+        return context
+
+class Price(TemplateView):
+    template_name = 'photo/price.html'
+    context_object_name = 'price'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Услуги и цены'
+        context['price_under_line'] = 'class="colorlib-active"'
+        return context
+
+class Collection(TemplateView):
+    template_name = 'photo/collection.html'
+    context_object_name = 'collection'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Работы'
+        context['collection_under_line'] = 'class="colorlib-active"'
+        return context
+
+class Contacts(TemplateView):
+    template_name = 'photo/contacts.html'
+    context_object_name = 'сontacts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Контакты'
+        context['contacts_under_line'] = 'class="colorlib-active"'
+        return context
+
+class About(TemplateView):
+    template_name = 'photo/about.html'
+    context_object_name = 'about'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Обо мне'
+        context['about_under_line'] = 'class="colorlib-active"'
         return context
